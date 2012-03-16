f590efbd6ed7c5d54600cb48ebcc4166aef66811
Vincent Tourraine <me@vtourraine.net>
2012-03-16 01:28:14 +0100

    Add comment (minor)

--- src/model/User.php
+++ src/model/User.php
@@ -8,6 +8,9 @@
         public $username = '';
         public $passwordMD5 = '';
         
+        /**
+        * setPassword
+        */
         public function setPassword($newPassword)
         {
             $this->passwordMD5 = md5($newPassword);
