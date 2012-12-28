142dbe4d96f65c843118541faf75b9e456a1b3ee
Vincent Tourraine <me@vtourraine.net>
2012-12-28 10:00:49 +0100

    Fix email content encoding

--- admin.php
+++ admin.php
@@ -17,7 +17,7 @@
           $remainingDays = date('t') - date('j');
           $body = '<p>Just a friendly reminder that you only have '.$remainingDays.' days left to select what’s best, and post it to <a href="http://'.str_replace('/admin.php', '', $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']).'">Must</a>.</p><p><i>Sent by a robot.</i></p>';
           $headers  = 'MIME-Version: 1.0'."\r\n";
-          $headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
+          $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
           $headers .= 'To: '.$recipient."\r\n";
           $headers .= 'From: Must <me@vtourraine.net>'."\r\n";
 
